9426ba3865a727db6dc97db672b4525b10707754
Gabriel Holanda <gabrielholanda.work@hotmail.com>
2026-02-20 11:19:00 -0300

    feat: implementa classe Conexao para conexão com MySQL

--- src/main/java/connection/Conexao.java
+++ src/main/java/connection/Conexao.java
@@ -1,5 +1,21 @@
 package connection;
 
-public class Conexao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class Conexao {
+	
+	private static final String URL = "jdbc:mysql://localhost:3306/vei_macho_do_queijo";
+    private static final String USER = "root";     
+    private static final String PASSWORD = "root";
+
+    public static Connection conectar() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao conectar com o banco de dados", e);
+        }
+    }
+	
 }
